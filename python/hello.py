b2dd2fac723861c60e29668587141d57af1a2c8d
kotsunia <erikamarz@outlook.de>
2025-10-10 23:00:26 +0100

    i have no clue tbh

--- python/hello.py
+++
@@ -1,3 +0,0 @@
-#asking for their name
-#says hello plus their name
-print("Hello,", input("What's your name? "))
---
+++ python/something.py
@@ -0,0 +1,43 @@
+# def salutations(name: str, age: int()):
+#     print(f"{name}, {age}")
+
+
+# salutations("Joao", 450.2)
+
+# names: list[str] = ['joao', "puta que pariu", "sergio", "o computador a queimar"]
+
+# for name in names:
+#     print(f"Salutations {name}")
+
+
+# idol: int = -45
+    
+# while idol < 3:
+#     print(idol)
+#     idol += 1
+    
+
+# a: int = 1
+# b: int = 2
+
+# print(a > b)
+# print(a >= b)
+# print(a != b)
+
+
+# while True:
+#     user_input: str = input()
+
+#     if user_input == 'salutations':
+#         print("HIIIIII <3")
+    
+#     elif user_input == "how are ye?":
+#         print("fucking shit")
+#     elif user_input == "bye":
+#         print("bye mf")
+#         exit()
+#     else:
+#         print("wtf did you say?")
+
+
+
