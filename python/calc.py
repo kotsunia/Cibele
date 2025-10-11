dff71dc5fb157aaeab2768a72ebc8a55a2dce4b6
kotsunia <erikamarz@outlook.de>
2025-10-11 12:50:35 +0100

    calcs

---
+++ python/calc.py
@@ -0,0 +1,22 @@
+# while True:
+#     calc: str = input("what do you want to calculate? ")
+#     print(eval(calc))
+
+# while True:
+#     Principal = float(input("Principal amount? "))
+#     Time = float(input("Time period (in years) "))
+#     Rate = float(input("Rate of interest per annum "))
+
+#     print("simple interest: ", Principal * Time * Rate / 100)
+
+# def interest(p ,t ,r):
+#     return (p * t * r) / 100
+
+
+# while True:
+
+#     p = float(input())
+#     t = float(input())
+#     r = float(input())
+
+#     print(interest (p, t, r))
