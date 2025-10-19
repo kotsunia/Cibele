723173829cd7e54005b760b7657565360f9eec0a
kotsunia <erikamarz@outlook.de>
2025-10-19 21:34:52 +0100

    Update automatetheboringstuff.py
    Signed-off-by: kotsunia <erikamarz@outlook.de>

---
+++ python/automatetheboringstuff.py
@@ -0,0 +1,98 @@
+# inshallah
+# ------------
+
+
+# name = input("Your name: ")
+
+# print("your name has this amount of letter: ", int(name))
+# ------
+
+# maths = int(input("how many breads have you eaten in your life? "))
+
+# print(f"YOU ATE HOW MUCH? YOU MANAGED TO EAT {maths} FULL SLICES OF BREAD???")
+# ------
+# a = 1
+# b = '2'
+# c = 400.3222
+
+# print(type(len(b)))
+# ------
+
+# idk why not working lmfao, i maybe be dumb at maths
+# floating = float(input("input a floating number please (it will round to 3 decimal places) "))
+
+# print(round(floating, 3))
+# ------
+# meth = eval(input())
+
+# print(meth)
+
+# user = "cambrao"
+# passw = "foda"
+
+# if user == "cambrao":
+#     print(f"hello {user}")
+#     if passw == "foda":
+#         print("vai se foder")
+# else:
+#     print("vai-te embora e se foda-te")
+
+
+# idk if i am an idiot
+
+# name = input("Whatcha name? ")
+# age = input("What's your age? ")
+
+# if name == 'Foda':
+#         print("text")
+# elif int(age) == 20:
+#     print("Nao podes entrar cralho")
+
+
+# today_is_opposite_day = oppo_day, say_it_is_opposite_day = isit_oppo_day IDK WHY NOT WORKING SOB
+# oppo_day = True
+
+
+# if oppo_day == True:
+#     isit_oppo_day = True
+# else:
+#     isit_oppo_day = False
+
+
+# if oppo_day == True:
+#     isit_oppo_day = not isit_oppo_day
+# if isit_oppo_day == True:
+#     print("Oppo day yes!")
+# else:
+#     print("no oppo day :[")
+
+
+# print(f"Enter GB/TB for the unit")
+# unit = input(">> ")
+
+# true_size = 0
+
+# if unit == 'TB' or unit == "tb":
+#     true_size = 1000000000000 / 1099511627776
+# elif unit == "GB" or unit == "gb":
+#     true_size = 1000000000 / 1073741824
+
+# print("AD Size")
+# ad_size = float(input(">>> "))
+
+# real_size = str(round(ad_size + true_size, 2))
+
+# print(f"blah blah {real_size + unit}")
+
+# bread = 7
+
+# while bread < 8:
+#     print("I LOVE BREAD <3")
+#     bread = bread + 1
+
+
+name = ""
+
+while name != 'namey':
+    print(input(name))
+print("thanks")
